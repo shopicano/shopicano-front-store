bdcc3632aa683f96899aa8d9421e08df5542431a
Abid <fmaabid@gmail.com>
2019-11-17 00:33:47 +0600

    - Added top product items

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,9 @@
                     <h2 class="section-title">Top Categories</h2>
                 </div>
                 <!-- categories item -->
-                <div v-for="category in categories" class="col-lg-4 col-md-6 mb-50">
+                <div v-for="category in categories"
+                     v-bind:key="category.id"
+                     class="col-lg-4 col-md-6 mb-50">
                     <div class="card p-0">
                         <div class="border-bottom text-center hover-zoom-img">
                             <router-link to="/shop">
@@ -38,16 +40,20 @@
                 <div class="col-lg-12 text-center">
                     <h2 class="section-title">Top Products</h2>
                 </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
+                <!-- Top product item -->
+                <div v-for="tp in topProducts"
+                     v-bind:key="tp.id"
+                     class="col-lg-4 col-md-6 mb-50">
                     <div class="card p-0">
                         <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-1.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
+                            <router-link to="/shop">
+                                <img class="rounded-top img-fluid imageDim" alt="product-img"
+                                     v-bind:src="getFullImagePath(tp.image)" >
+                            </router-link>
                         </div>
                         <ul class="d-flex list-unstyled pl-0 categories-list">
                             <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-1.jpg"
+                                <a href="shop.html"><img v-bind:src="getFullImagePath(tp.image)"
                                                          class="img-fluid w-100" alt="product-img"></a>
                             </li>
                             <li class="m-0 hover-zoom-img">
@@ -60,223 +66,7 @@
                             </li>
                         </ul>
                         <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Clothing</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-2.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-4.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-5.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-6.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Shoes</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-3.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-7.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-8.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-9.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Accessories</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-1.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-1.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-2.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-3.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Clothing</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-2.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-4.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-5.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-6.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Shoes</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-3.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-7.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-8.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-9.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Accessories</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-1.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-1.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-2.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-3.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Clothing</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-2.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-4.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-5.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-6.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Shoes</h4>
-                            <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- categories item -->
-                <div class="col-lg-4 col-md-6 mb-50">
-                    <div class="card p-0">
-                        <div class="border-bottom text-center hover-zoom-img">
-                            <a href="shop.html"><img src="images/categories/product-big-3.jpg"
-                                                     class="rounded-top img-fluid w-100" alt="product-img"></a>
-                        </div>
-                        <ul class="d-flex list-unstyled pl-0 categories-list">
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-7.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-8.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                            <li class="m-0 hover-zoom-img">
-                                <a href="shop.html"><img src="images/categories/product-sm-9.jpg"
-                                                         class="img-fluid w-100" alt="product-img"></a>
-                            </li>
-                        </ul>
-                        <div class="px-4 py-3 border-top">
-                            <h4 class="d-inline-block mb-0 mt-1">Accessories</h4>
+                            <h4 class="d-inline-block mb-0 mt-1">{{ tp.name }}</h4>
                             <a href="shop.html" class="btn btn-sm btn-outline-primary float-right">view more</a>
                         </div>
                     </div>
@@ -296,16 +86,24 @@
         data() {
             return {
                 categories: [],
+                topProducts: [],
             };
         },
         mounted() {
             this.getCategories();
+            this.getProducts();
         },
         methods: {
             getCategories: function () {
                 axios.get( Settings.GetApiUrl() + '/categories?limit=3').then(resp => {
-                    console.log(resp);
                     this.categories = resp.data.data;
+                }).catch(err => {
+                    console.log(err);
+                })
+            },
+            getProducts: function () {
+                axios.get( Settings.GetApiUrl() + '/products?limit=9').then(resp => {
+                    this.topProducts = resp.data.data;
                 }).catch(err => {
                     console.log(err);
                 })

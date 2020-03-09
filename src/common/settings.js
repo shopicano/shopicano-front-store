93695527f465018e30b76ffdc5598be7d11bc9e7
fmabid <fmaabid@gmail.com>
2020-03-09 20:09:22 +0600

    - Added : Payment processing fee

--- src/common/settings.js
+++ src/common/settings.js
@@ -1,7 +1,7 @@
 class Settings {
 
     static GetApiUrl() {
-        return process.env.VUE_APP_SHOPICANO_URL + "/v1";
+        return "http://128.199.89.247:9119/v1";
     }
 
     static GetMediaUrl() {
